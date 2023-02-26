547f5e760dcaa3ee8e96b24cea0bad7edece2104
Seyoung Jung <55081331+Seyoung-Jung@users.noreply.github.com>
2023-02-26 22:33:16 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 2128 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
@@ -1,5 +1,3 @@
-import math
-
 N = [i for i in range(2, 10001)]
 Primes = []
 while N[0] < (10001 ** 0.5 + 1):
@@ -12,9 +10,9 @@ Primes.extend(N)
 
 T = int(input())
 Goldbach = []
-for t in range(T):
+for _ in range(T):
     n = int(input())
-    gold_num = math.ceil(n // 2)
+    gold_num = n // 2
     while True:
         if gold_num in Primes and (n - gold_num) in Primes:
             Goldbach.append([(n - gold_num), gold_num])
